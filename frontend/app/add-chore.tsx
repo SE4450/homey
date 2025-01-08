a24b7762eac5f243e4742b4a11e5f64fc53e3956
aarishlakhani <alakha33@uwo.ca>
2025-01-08 00:49:04 -0500

    create new screens

---
+++ frontend/app/add-chore.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+const AddChore = () => {
+  return (
+    <View>
+      <Text>AddChore</Text>
+    </View>
+  );
+};
+
+export default AddChore;
+
+const styles = StyleSheet.create({});
---
+++ frontend/app/daily-expenses.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+const DailyExpenses = () => {
+  return (
+    <View>
+      <Text>DailyExpenses</Text>
+    </View>
+  );
+};
+
+export default DailyExpenses;
+
+const styles = StyleSheet.create({});

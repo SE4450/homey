1e31dfbd5b786a0da43145bc5726e852310888c8
AndrewDevito <adevito4@uwo.ca>
2025-01-19 11:09:35 -0500

    restricted calendar requests to authenticated users

--- backend/routes/eventRoutes.js
+++ backend/routes/eventRoutes.js
@@ -3,12 +3,9 @@ const router = express();
 const { getEvents, createEvent, updateEvent, deleteEvent } = require("../controllers/eventController.js");
 const { authenticateUser } = require("../middleware/authenticateUser");
 
-router.get("/", getEvents);
-
-router.post("/", createEvent);
-
-router.post("/updateEvent", updateEvent);
-
-router.post("/deleteEvent", deleteEvent);
+router.get("/", authenticateUser(["tenant", "landlord"]), getEvents);
+router.post("/", authenticateUser(["tenant", "landlord"]), createEvent);
+router.post("/updateEvent", authenticateUser(["tenant", "landlord"]), updateEvent);
+router.post("/deleteEvent", authenticateUser(["tenant", "landlord"]), deleteEvent);
 
 module.exports = router;

157b0d197e24f8aee27823f99821703c21da8fce
aarishlakhani <50451618+aarishlakhani@users.noreply.github.com>
2025-03-18 00:31:41 -0400

    Frontend Lists, List Display and Profile (#22)
    * backend for chores and add, delete chores
    
    * fixed key related problems
    
    * user id
    
    * complete, incomplete chores
    
    * Due date
    
    * due date backend and frontend, set tiles asc order
    
    * dueDate Details Frontend
    
    * fixed navigation tabs
    
    * Fixed Container issues in choreDetails and scrolling in Chores
    
    * Fixed Chore Id Bug
    
    * Divide Chore Page, Fix All Chore Related Issues
    
    * Frontend Improvements for List
    
    * Remove Store Search and Add List Label
    
    * More frontend changes and improvements
    
    * Final Touches to Lists
    
    * Profile Page UI Improvements
    
    * Merge Conflicts Resolve

--- frontend/app/groupNavigation.tsx
+++ frontend/app/groupNavigation.tsx
@@ -8,12 +8,12 @@ import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { MaterialIcons } from "@expo/vector-icons";
 
 import HomeScreen from "./home";
-import ProfileScreen from './profile'
-import ListScreen from './listDisplay';
-import ExpenseScreen from './expenses';
+import ProfileScreen from "./profile";
+import ListScreen from "./listDisplay";
+import ExpenseScreen from "./expenses";
 import MessageStackScreen from "./stacks/messagesStack";
-import InventoryScreen from './inventory'
-import CalendarScreen from './calendar'
+import InventoryScreen from "./inventory";
+import CalendarScreen from "./calendar";
 import ChoresStackScreen from "./stacks/choresStack";
 import ReviewsScreen from "./stacks/reviewsStack";
 
@@ -69,8 +69,6 @@ export default function GroupNavigationScreen() {
 
           if (route.name === "Home") {
             iconName = "home";
-          } else if (route.name === "Profile") {
-            iconName = "account-circle";
           } else if (route.name === "List") {
             iconName = "shopping-cart";
           } else if (route.name === "Expenses") {
@@ -81,13 +79,13 @@ export default function GroupNavigationScreen() {
             iconName = "message";
           } else if (route.name === "Inventory") {
             iconName = "inventory";
-          } else if (route.name === "Reviews"){
+          } else if (route.name === "Profile") {
+            iconName = "account-circle";
+          } else if (route.name === "Reviews") {
             iconName = "star-rate";
-          }
-          else if (route.name === 'Calendar') {
-            iconName = 'calendar-today';
-          }
-          else {
+          } else if (route.name === "Calendar") {
+            iconName = "calendar-today";
+          } else {
             iconName = "help"; // Fallback icon
           }
 
